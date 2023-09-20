6903057b27c3f11375abaaeccb7e549cc718579f
Yash <thakkaryash458@gmail.com>
2023-09-21 05:08:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+ const App = () => {
+  return (
+    <div>
+        <div>
+          <h1>App</h1>
+          <div></div>
+          
+        </div>
+    
+    </div>
+  )
+}
+
+
+export default App;
